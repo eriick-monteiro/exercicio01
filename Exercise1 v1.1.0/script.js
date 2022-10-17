1326a50263d1fe41654b094940e43deb72b8207b
Erick Monteiro <115753972+eriick-monteiro@users.noreply.github.com>
2022-10-17 11:48:34 -0300

    Update script.js

--- Exercise1 v1.1.0/script.js
+++ Exercise1 v1.1.0/script.js
@@ -1,9 +1,14 @@
-
 function vol() {
 	var corpo = window.document.body;
+	var textos = window.document.body.h1;
 	var red = document.getElementById('volume0').value
 	var grn = document.getElementById('volume1').value
 	var blu = document.getElementById('volume2').value
 	
+	var tred = document.getElementById('txt0').value
+	var tgrn = document.getElementById('txt1').value
+	var tblu = document.getElementById('txt2').value
+	
 	document.body.style.background = "rgb(" + red + "," + grn + "," + blu + ")";
+	document.body.style.color = "rgb(" + tred + "," + tgrn + "," + tblu + ")";
 }
