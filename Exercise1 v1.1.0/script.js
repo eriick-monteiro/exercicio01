dbebe887d3eb4730b8ae76d53772548aff9cf1f3
Erick Monteiro <115753972+eriick-monteiro@users.noreply.github.com>
2022-10-17 12:33:44 -0300

    Update script.js

--- Exercise1 v1.1.0/script.js
+++ Exercise1 v1.1.0/script.js
@@ -1,5 +1,6 @@
 
-function vol() {
+function changeColor() {
+	//Coletando os dados dos Inputs
 	var bgred = document.getElementById('bgred');
 	var txred = document.getElementById('txred');
 	var bggrn = document.getElementById('bggrn');
@@ -7,20 +8,24 @@ function vol() {
 	var bgblu = document.getElementById('bgblu');
 	var txblu = document.getElementById('txblu');
 	
+	//Pegando dados do Body
 	var corpo = window.document.body;
-	var red = document.getElementById('volume0').value
-	var grn = document.getElementById('volume1').value
-	var blu = document.getElementById('volume2').value
 	
-	var tred = document.getElementById('txt0').value
-	var tgrn = document.getElementById('txt1').value
-	var tblu = document.getElementById('txt2').value
+	//Coletando dados em Números dos 3 inputs que irão definir a cor do Background
+	var bred = document.getElementById('bg0').value
+	var bgrn = document.getElementById('bg1').value
+	var bblu = document.getElementById('bg2').value
 	
-	//document.body.style.background = "rgb(" + red + "," + grn + "," + blu + ")";
-	document.body.style.background = `rgb(${red}, ${grn}, ${blu})`;
-	//document.body.style.color = "rgb(" + tred + "," + tgrn + "," + tblu + ")";
+	//Coletando dados em Números dos 3 inputs que irão definir a cor do Texto
+	var tred = document.getElementById('tx0').value
+	var tgrn = document.getElementById('tx1').value
+	var tblu = document.getElementById('tx2').value
+	
+	//Aqui estamos definindo a cor do Background e dos Textos baseados nos dados recebidos dos inputs de Range
+	document.body.style.background = `rgb(${bred}, ${bgrn}, ${bblu})`;
 	document.body.style.color = `rgb(${tred}, ${tgrn}, ${tblu})`;
 
+	//Escrevendo o número de cada cor em RGB ao lado de cada Input de Range
 	bgred.innerText = `${red}`;
 	txred.innerText = `${tred}`;
 	bggrn.innerText = `${grn}`;
