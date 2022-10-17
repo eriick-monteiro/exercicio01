dacc54927c89da8c2604ee0c6e96144635479e2a
Erick Monteiro <115753972+eriick-monteiro@users.noreply.github.com>
2022-10-17 12:24:13 -0300

    Update script.js

--- Exercise1 v1.1.0/script.js
+++ Exercise1 v1.1.0/script.js
@@ -1,6 +1,13 @@
+
 function vol() {
+	var bgred = document.getElementById('bgred');
+	var txred = document.getElementById('txred');
+	var bggrn = document.getElementById('bggrn');
+	var txgrn = document.getElementById('txgrn');
+	var bgblu = document.getElementById('bgblu');
+	var txblu = document.getElementById('txblu');
+	
 	var corpo = window.document.body;
-	var textos = window.document.body.h1;
 	var red = document.getElementById('volume0').value
 	var grn = document.getElementById('volume1').value
 	var blu = document.getElementById('volume2').value
@@ -9,6 +16,15 @@ function vol() {
 	var tgrn = document.getElementById('txt1').value
 	var tblu = document.getElementById('txt2').value
 	
-	document.body.style.background = "rgb(" + red + "," + grn + "," + blu + ")";
-	document.body.style.color = "rgb(" + tred + "," + tgrn + "," + tblu + ")";
+	//document.body.style.background = "rgb(" + red + "," + grn + "," + blu + ")";
+	document.body.style.background = `rgb(${red}, ${grn}, ${blu})`;
+	//document.body.style.color = "rgb(" + tred + "," + tgrn + "," + tblu + ")";
+	document.body.style.color = `rgb(${tred}, ${tgrn}, ${tblu})`;
+
+	bgred.innerText = `${red}`;
+	txred.innerText = `${tred}`;
+	bggrn.innerText = `${grn}`;
+	txgrn.innerText = `${tgrn}`;
+	bgblu.innerText = `${blu}`;
+	txblu.innerText = `${tblu}`;
 }
